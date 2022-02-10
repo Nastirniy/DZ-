a998ab90931bcab7bcc2d779f63570f5ab170464
Nastirniy <73361125+Nastirniy@users.noreply.github.com>
2022-02-10 14:29:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите двузначное число: ");
+string? vvod_chisla = Console.ReadLine();
+
+char num1 = vvod_chisla[0];
+char num2 = vvod_chisla[1];
+
+if (num1 == num2)
+    Console.WriteLine($"{num1} = {num2}");
+else if (num1 > num2)
+    Console.WriteLine($"{num1} > {num2}");
+else
+    Console.WriteLine($"{num1} < {num2}");
